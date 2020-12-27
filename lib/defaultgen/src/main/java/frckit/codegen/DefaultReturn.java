7e077b7e52ff6f0f42a93f78b3f4caa8c07cef63
Cameron Earle <camearle20@me.com>
2020-12-27 15:49:52 -0500

    Added implementation of defaultgen

---
+++ lib/defaultgen/src/main/java/frckit/codegen/DefaultReturn.java
@@ -0,0 +1,12 @@
+package frckit.codegen;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.CLASS)
+public @interface DefaultReturn {
+    String value();
+}
