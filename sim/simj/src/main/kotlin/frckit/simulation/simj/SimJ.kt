c4c0686a3c9b2bf77dfa7b689be500b8339f8373
Cameron Earle <camearle20@me.com>
2021-01-19 01:19:56 -0500

    Engine testing

--- sim/simj/src/main/kotlin/frckit/simulation/simj/SimJ.kt
+++ sim/simj/src/main/kotlin/frckit/simulation/simj/SimJ.kt
@@ -1,17 +1,21 @@
 package frckit.simulation.simj
 
 import javafx.application.Application
+import javafx.application.ConditionalFeature
+import javafx.application.Platform
 import javafx.scene.Scene
 import javafx.scene.control.Button
 import javafx.scene.layout.Pane
+import javafx.scene.shape.Box
+import javafx.scene.transform.Rotate
 import javafx.stage.Stage
 
 class SimJ: Application() {
     override fun start(primaryStage: Stage) {
-        val pane = Pane(Button("Test"))
-        val scene = Scene(pane)
-        primaryStage.scene = scene
-        primaryStage.show()
+        println(Platform.isSupported(ConditionalFeature.SCENE3D))
+
+        val box = Box()
+        val test = Rotate()
     }
 }
 
