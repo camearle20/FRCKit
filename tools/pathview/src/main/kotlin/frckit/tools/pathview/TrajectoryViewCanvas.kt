16ed1f9374b7356a1fe661b6ae4cfad43e8b30de
Cameron Earle <camearle20@me.com>
2021-02-17 18:05:49 -0500

    Added support for dragging view with mouse.  Version 0.0.28

--- tools/pathview/src/main/kotlin/frckit/tools/pathview/TrajectoryViewCanvas.kt
+++ tools/pathview/src/main/kotlin/frckit/tools/pathview/TrajectoryViewCanvas.kt
@@ -5,6 +5,9 @@ import edu.wpi.first.wpilibj.geometry.Translation2d
 import edu.wpi.first.wpilibj.trajectory.Trajectory
 import frckit.util.GeomUtil
 import java.awt.*
+import java.awt.event.MouseEvent
+import java.awt.event.MouseListener
+import java.awt.event.MouseMotionListener
 import java.lang.RuntimeException
 import java.text.DecimalFormat
 import java.util.concurrent.ScheduledThreadPoolExecutor
@@ -17,13 +20,22 @@ import kotlin.math.ceil
 import kotlin.math.roundToInt
 import kotlin.math.roundToLong
 
-class TrajectoryViewCanvas(val ppi: Double, val fieldWidth: Double, val fieldHeight: Double, val trajectory: Trajectory, val trackWidthInches: Double, val markers: List<Translation2d>): JPanel(true) {
-    private fun horizontalInchesToPixels(inches: Double): Int {
-        return width - (ppi * inches).roundToInt()
+class TrajectoryViewCanvas(val ppm: Double, val fieldWidth: Double, val fieldHeight: Double, val trajectory: Trajectory, val trackWidthMeters: Double, val markers: List<Translation2d>): JPanel(true) {
+    private var viewOffsetX = 0
+    private var viewOffsetY = 0
+
+    fun resetView() {
+        viewOffsetX = 0
+        viewOffsetY = 0
+        repaint()
     }
 
-    private fun verticalInchesToPixels(inches: Double): Int {
-        return height - (ppi * inches).roundToInt()
+    private fun horizontalMetersToPixels(meters: Double): Int {
+        return width - (ppm * meters).roundToInt() + viewOffsetX
+    }
+
+    private fun verticalMetersToPixels(meters: Double): Int {
+        return height - (ppm * meters).roundToInt() + viewOffsetY
     }
 
     data class TrajectoryStats(val maxVel: Double, val time: Double)
@@ -35,8 +47,8 @@ class TrajectoryViewCanvas(val ppi: Double, val fieldWidth: Double, val fieldHei
             val state = trajectory.states[i]
             val dx = state.velocityMetersPerSecond
             val dtheta = state.velocityMetersPerSecond * state.curvatureRadPerMeter
-            val leftVelocity = abs(dx - (trackWidthInches / 2.0 * dtheta)) //Forward kinematics
-            val rightVelocity = abs(dx + (trackWidthInches / 2.0 * dtheta))
+            val leftVelocity = abs(dx - (trackWidthMeters / 2.0 * dtheta)) //Forward kinematics
+            val rightVelocity = abs(dx + (trackWidthMeters / 2.0 * dtheta))
             if (leftVelocity > maxVel) maxVel = leftVelocity
             if (rightVelocity > maxVel) maxVel = rightVelocity
         }
@@ -99,15 +111,15 @@ class TrajectoryViewCanvas(val ppi: Double, val fieldWidth: Double, val fieldHei
 
     private val stats = computeStats()
     private val pathStroke = BasicStroke(2.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL)
-    private val leftTrackTransform = Transform2d(Translation2d(0.0, (trackWidthInches) / 2.0), GeomUtil.IDENTITY_ROTATION)
-    private val rightTrackTransform = Transform2d(Translation2d(0.0, (trackWidthInches) / -2.0), GeomUtil.IDENTITY_ROTATION)
-    private val frontTransform = Transform2d(Translation2d((trackWidthInches) / 2.0, 0.0), GeomUtil.IDENTITY_ROTATION)
-    private val backTransform = Transform2d(Translation2d((trackWidthInches) / -2.0, 0.0), GeomUtil.IDENTITY_ROTATION)
+    private val leftTrackTransform = Transform2d(Translation2d(0.0, (trackWidthMeters) / 2.0), GeomUtil.IDENTITY_ROTATION)
+    private val rightTrackTransform = Transform2d(Translation2d(0.0, (trackWidthMeters) / -2.0), GeomUtil.IDENTITY_ROTATION)
+    private val frontTransform = Transform2d(Translation2d((trackWidthMeters) / 2.0, 0.0), GeomUtil.IDENTITY_ROTATION)
+    private val backTransform = Transform2d(Translation2d((trackWidthMeters) / -2.0, 0.0), GeomUtil.IDENTITY_ROTATION)
 
     private fun drawRobot(g: Graphics2D) {
         val currentPose = activeSimulation.latestState
-        val horiz = horizontalInchesToPixels(currentPose.translation.y)
-        val vert = verticalInchesToPixels(currentPose.translation.x)
+        val horiz = horizontalMetersToPixels(currentPose.translation.y)
+        val vert = verticalMetersToPixels(currentPose.translation.x)
 
         //Draw the dot on the robot origin
 
@@ -120,14 +132,14 @@ class TrajectoryViewCanvas(val ppi: Double, val fieldWidth: Double, val fieldHei
         val robotBackLeft = currentPose.transformBy(backTransform).transformBy(leftTrackTransform)
         val robotBackRight = currentPose.transformBy(backTransform).transformBy(rightTrackTransform)
 
-        val frontLeftHoriz = horizontalInchesToPixels(robotFrontLeft.translation.y)
-        val frontLeftVert = verticalInchesToPixels(robotFrontLeft.translation.x)
-        val frontRightHoriz = horizontalInchesToPixels(robotFrontRight.translation.y)
-        val frontRightVert = verticalInchesToPixels(robotFrontRight.translation.x)
-        val backLeftHoriz = horizontalInchesToPixels(robotBackLeft.translation.y)
-        val backLeftVert = verticalInchesToPixels(robotBackLeft.translation.x)
-        val backRightHoriz = horizontalInchesToPixels(robotBackRight.translation.y)
-        val backRightVert = verticalInchesToPixels(robotBackRight.translation.x)
+        val frontLeftHoriz = horizontalMetersToPixels(robotFrontLeft.translation.y)
+        val frontLeftVert = verticalMetersToPixels(robotFrontLeft.translation.x)
+        val frontRightHoriz = horizontalMetersToPixels(robotFrontRight.translation.y)
+        val frontRightVert = verticalMetersToPixels(robotFrontRight.translation.x)
+        val backLeftHoriz = horizontalMetersToPixels(robotBackLeft.translation.y)
+        val backLeftVert = verticalMetersToPixels(robotBackLeft.translation.x)
+        val backRightHoriz = horizontalMetersToPixels(robotBackRight.translation.y)
+        val backRightVert = verticalMetersToPixels(robotBackRight.translation.x)
 
         g.drawLine(frontLeftHoriz, frontLeftVert, frontRightHoriz, frontRightVert) //front left -> front right
         g.drawLine(frontRightHoriz, frontRightVert, backRightHoriz, backRightVert) //front right -> back right
@@ -141,27 +153,48 @@ class TrajectoryViewCanvas(val ppi: Double, val fieldWidth: Double, val fieldHei
 
     private fun drawText(g: Graphics2D) {
         val time = fmt.format(activeSimulation.latestTime)
+        val totalTime = fmt.format(trajectory.totalTimeSeconds)
         val state = activeSimulation.latestState
 
         g.color = Color.BLACK
-        g.drawString("time: $time s", 10, 20)
+        g.drawString("time - current: $time s,   total: $totalTime s", 10, 20)
         g.drawString("pose: $state", 10, 40)
 
         g.paint = velGradient
         g.fillRect(10, 60, 120, 10)
 
         g.color = Color.BLACK
-        g.drawString("${fmt.format(stats.maxVel)} in/s", 135, 70)
+        g.drawString("${fmt.format(stats.maxVel)} m/s", 135, 70)
     }
 
     private fun drawMarkers(g: Graphics2D) {
         g.color = Color.MAGENTA
 
-        val offset = ((7 / 2) * ppi).roundToInt()
-        val size = (7 * ppi).roundToInt()
+        val offset = ((0.1778 / 2) * ppm).roundToInt()
+        val size = (0.1778 * ppm).roundToInt()
         markers.forEach {
-            g.fillOval(horizontalInchesToPixels(it.y) - offset, verticalInchesToPixels(it.x) - offset, size, size)
+            g.fillOval(horizontalMetersToPixels(it.y) - offset, verticalMetersToPixels(it.x) - offset, size, size)
         }
+    }
+
+    private fun drawOrigin(g: Graphics2D) {
+        g.color = Color.RED
+
+        g.drawLine(
+            horizontalMetersToPixels(0.0),
+            verticalMetersToPixels(0.0),
+            horizontalMetersToPixels(0.0),
+            verticalMetersToPixels(0.5)
+        )
+
+        g.color = Color.GREEN
+
+        g.drawLine(
+            horizontalMetersToPixels(0.0),
+            verticalMetersToPixels(0.0),
+            horizontalMetersToPixels(0.5),
+            verticalMetersToPixels(0.0)
+        )
     }
 
     private fun drawTrajectory(g: Graphics2D) {
@@ -178,25 +211,25 @@ class TrajectoryViewCanvas(val ppi: Double, val fieldWidth: Double, val fieldHei
             val curStateRight = curPose.transformBy(rightTrackTransform)
             val lastStateRight = lastPose.transformBy(rightTrackTransform)
 
-            val curHoriz = horizontalInchesToPixels(curState.poseMeters.translation.y)
-            val curVert = verticalInchesToPixels(curState.poseMeters.translation.x)
-            val lastHoriz = horizontalInchesToPixels(lastState.poseMeters.translation.y)
-            val lastVert = verticalInchesToPixels(lastState.poseMeters.translation.x)
+            val curHoriz = horizontalMetersToPixels(curState.poseMeters.translation.y)
+            val curVert = verticalMetersToPixels(curState.poseMeters.translation.x)
+            val lastHoriz = horizontalMetersToPixels(lastState.poseMeters.translation.y)
+            val lastVert = verticalMetersToPixels(lastState.poseMeters.translation.x)
 
-            val curLeftHoriz = horizontalInchesToPixels(curStateLeft.translation.y)
-            val curLeftVert = verticalInchesToPixels(curStateLeft.translation.x)
-            val lastLeftHoriz = horizontalInchesToPixels(lastStateLeft.translation.y)
-            val lastLeftVert = verticalInchesToPixels(lastStateLeft.translation.x)
+            val curLeftHoriz = horizontalMetersToPixels(curStateLeft.translation.y)
+            val curLeftVert = verticalMetersToPixels(curStateLeft.translation.x)
+            val lastLeftHoriz = horizontalMetersToPixels(lastStateLeft.translation.y)
+            val lastLeftVert = verticalMetersToPixels(lastStateLeft.translation.x)
 
-            val curRightHoriz = horizontalInchesToPixels(curStateRight.translation.y)
-            val curRightVert = verticalInchesToPixels(curStateRight.translation.x)
-            val lastRightHoriz = horizontalInchesToPixels(lastStateRight.translation.y)
-            val lastRightVert = verticalInchesToPixels(lastStateRight.translation.x)
+            val curRightHoriz = horizontalMetersToPixels(curStateRight.translation.y)
+            val curRightVert = verticalMetersToPixels(curStateRight.translation.x)
+            val lastRightHoriz = horizontalMetersToPixels(lastStateRight.translation.y)
+            val lastRightVert = verticalMetersToPixels(lastStateRight.translation.x)
 
-            val dx = curState.velocityMetersPerSecond // in/s
+            val dx = curState.velocityMetersPerSecond
             val dtheta = curState.velocityMetersPerSecond * curState.curvatureRadPerMeter
-            val leftVelocity = abs(dx - (trackWidthInches / 2.0 * dtheta))
-            val rightVelocity = abs(dx + (trackWidthInches / 2.0 * dtheta))
+            val leftVelocity = abs(dx - (trackWidthMeters / 2.0 * dtheta))
+            val rightVelocity = abs(dx + (trackWidthMeters / 2.0 * dtheta))
 
             val leftWeight = abs(leftVelocity) / stats.maxVel
             val rightWeight = abs(rightVelocity) / stats.maxVel
@@ -211,11 +244,20 @@ class TrajectoryViewCanvas(val ppi: Double, val fieldWidth: Double, val fieldHei
         }
     }
 
+    //Mouse state
+    private var isDragging = false
+    private var dragStartX = 0
+    private var dragStartY = 0
+    private var dragStartOffsetX = 0
+    private var dragStartOffsetY = 0
+
+
     //Main rendering function
     override fun paintComponent(g: Graphics) {
         super.paintComponent(g)
         val g2d = g as Graphics2D
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
+        drawOrigin(g2d)
         drawTrajectory(g2d)
         drawRobot(g2d)
         drawText(g2d)
@@ -231,8 +273,46 @@ class TrajectoryViewCanvas(val ppi: Double, val fieldWidth: Double, val fieldHei
     }
 
     init {
-        preferredSize = Dimension(ceil(fieldWidth * ppi).toInt(), ceil(fieldHeight * ppi).toInt())
+        preferredSize = Dimension(ceil(fieldWidth * ppm).toInt(), ceil(fieldHeight * ppm).toInt())
         border = BorderFactory.createLineBorder(Color.BLACK, 1)
         resetSimulation()
+        addMouseListener(object : MouseListener {
+            override fun mouseClicked(e: MouseEvent?) {
+
+            }
+
+            override fun mousePressed(e: MouseEvent) {
+                dragStartX = e.x
+                dragStartY = e.y
+                dragStartOffsetX = viewOffsetX
+                dragStartOffsetY = viewOffsetY
+            }
+
+            override fun mouseReleased(e: MouseEvent?) {
+            }
+
+            override fun mouseEntered(e: MouseEvent?) {
+            }
+
+            override fun mouseExited(e: MouseEvent?) {
+            }
+
+        })
+
+        addMouseMotionListener(object : MouseMotionListener {
+            override fun mouseDragged(e: MouseEvent) {
+                val dx = e.x - dragStartX
+                val dy = e.y - dragStartY
+
+                viewOffsetX = dragStartOffsetX + dx
+                viewOffsetY = dragStartOffsetY + dy
+
+                repaint()
+            }
+
+            override fun mouseMoved(e: MouseEvent?) {
+            }
+
+        })
     }
 }
